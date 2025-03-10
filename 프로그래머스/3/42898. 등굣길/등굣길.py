cde2e327a7c7031d27e2f090275782f7f4218ff4
BrightAsh <90810745+BrightAsh@users.noreply.github.com>
2025-03-10 16:38:36 +0900

    [level 3] Title: 등굣길, Time: 4.56 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(m, n, puddles):
+    dp=[[0]*n for _ in range(m)]
+    dp[0][0]=1
+    for i in range(m):
+        for j in range(n):
+            if [i+1,j+1] in puddles:
+                dp[i][j] = 0 
+            else:
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+                
+    return dp[-1][-1] % 1000000007
